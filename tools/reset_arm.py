bbe268ed80027a0fb4d804644d63a06789b7593c
William <1337842463@qq.com>
2026-02-27 15:02:17 +0800

    arm reset

--- tools/reset_arm.py
+++ tools/reset_arm.py
@@ -3,12 +3,10 @@ import time
 import json
 from pathlib import Path
 import sys
+from third_party.RM_API2.Python.Robotic_Arm.rm_robot_interface import *
 
-sys.path.insert(0, str(Path(__file__).parent.parent))
-from src.vision.vision_robot_bridge import VisionRobotBridge
-
-bridge = VisionRobotBridge()
-arm = bridge.robot.arm
+arm = RoboticArm(rm_thread_mode_e.RM_TRIPLE_MODE_E)
+handle = arm.rm_create_robot_arm("10.168.1.18", 8080)
 
 reset_joints_deg = [35.635, -41.329, -99.03, -62.103, 71.107, 154.35]
 ret = arm.rm_movej(reset_joints_deg, 5, 0, 0, 1) #速度比例，交融半径百分比系数，轨迹连接标志，阻塞设置（1为阻塞）
